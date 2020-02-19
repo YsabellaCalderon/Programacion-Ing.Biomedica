cbee6240a1d738c90f55ad85f26c06a0472ff03f
YsabellaCalderon <dariannac.briceno@gmail.com>
2020-02-19 12:11:38 -0500

    holamundo

---
+++ Clases/Clase 1/holamundo.py
@@ -0,0 +1,23 @@
+#codigo primera clase
+nombre = "Ysa"
+print (nombre)
+print (type (nombre))
+edad = 19
+print (edad)
+print (type (edad))
+dinero = 120.0
+print (dinero)
+print (type (dinero))
+#codigo tarea empresa
+nombre = "artscase"
+print (nombre)
+print (type(nombre))
+antiguedad = 5
+print (antiguedad)
+print (type(antiguedad))
+ganancia = 998.8
+print (ganancia)
+print (type(ganancia))
+
+
+

f31bc6df1048448a8cab935f037fdf6defc291c6
YsabellaCalderon <dariannac.briceno@gmail.com>
2020-03-13 10:42:06 -0500

    Calculadora

--- Clases/Clase 7/calculadora.py
+++ Clases/Clase 7/calculadora.py
@@ -6,20 +6,21 @@
 # multiplicar
 # dividir
 #return: devolver
-
+#--------------------------------------#
+#--------------Mensajes----------------#
 MENSAJE_NUMERO = "ingrese el numero deseado"
 MENSAJE_NUMERO_2 = "ingrese el numero deseado"
 MENSAJE_SUMA = "la suma de estos dos numeros es"
 MENSAJE_RESTA = "la resta de estos dos mnumeros es"
 MENSAJE_MULTIPLICACION = "la multiplicacion de estos dos numeros es"
 MENSAJE_DIVISION = "la division de estos dos numeros es"
-#------------------------------#
-#-----------Entradas-----------#
+#--------------Mensajes----------------#
+#--------------Entradas----------------#
 _numeroDeseado1 = 0
 _numeroDeseado2 = 0
-#------------Variable----------#
+#--------------Variable----------------#
 resultado = 0
-#-----------Codigo-------------#
+#--------------Codigo------------------#
 
 _numeroDeseado1 = int(input(MENSAJE_NUMERO))
 _numeroDeseado2 = int(input(MENSAJE_NUMERO_2))

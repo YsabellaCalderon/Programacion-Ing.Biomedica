beee0b41698f4b8eae68e7ee1634bcaf9ea84825
YsabellaCalderon <dariannac.briceno@gmail.com>
2020-03-18 08:50:11 -0500

    Clases Humano

--- Clases/Clase 7/Clases_humano.py
+++ Clases/Clase 7/Clases_humano.py
@@ -6,21 +6,29 @@ class Humano () :      #clase#
         self.nombre = nombre
         self.estatura = 1.60
         self.estatura_2 = 1.65 
+        self.estatura_3 = 1.55
         self.peso = 56
         self.peso_2 = 55
+        self.peso_3 = 58
         self.genero = "femenino"
 
 
 ser_humano = Humano ("ysabella")  #objeto#
-ser_humano_humano_2 = Humano ("valeria")
+ser_humano_2 = Humano ("valeria")
+ser_humano_3 = Humano ("Marella")
 print (ser_humano.raza)
 print (ser_humano.nombre)
 print (ser_humano.estatura)
 print (ser_humano.peso)
 print (ser_humano.genero)
-print (ser_humano_humano_2.raza)
-print (ser_humano_humano_2.nombre)
-print (ser_humano_humano_2.estatura_2)
-print (ser_humano_humano_2.peso_2)
-print (ser_humano_humano_2.genero)
+print (ser_humano_2.raza)
+print (ser_humano_2.nombre)
+print (ser_humano_2.estatura_2)
+print (ser_humano_2.peso_2)
+print (ser_humano_2.genero)
+print (ser_humano_3.raza)
+print (ser_humano_3.nombre)
+print (ser_humano_3.estatura_3)
+print (ser_humano_3.peso_3)
+print (ser_humano_3.genero)
 

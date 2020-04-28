85ea74c2cae31dee98aa643ef29ac8975c762dd3
YsabellaCalderon <dariannac.briceno@gmail.com>
2020-04-28 11:32:25 -0500

    Pandas

--- Clases/Tarea_pandas.py
+++ Clases/Tarea_pandas.py
@@ -1,5 +1,6 @@
 import pandas as p
 diccionario =p.read_csv("balance.csv",encoding = 'UTF-8',header = 0,delimiter=';').to_dict()
+print (diccionario)
 
 nombre_largo = max (diccionario["Ciudad"].values(),key = len)
 nombre_corto = min (diccionario["Ciudad"].values(),key = len)
@@ -7,6 +8,6 @@ print ("\n\n La ciudad con el nombre ,mas largo es {} y la ciudad con el nombre 
 .format(nombre_largo,nombre_corto))
 
 mayor_ganancias = max (diccionario["Ganancias"].values())
-menor_ganancias = min (diccionario["Perdidas"].values())
+menor_ganancias = max (diccionario["Perdidas"].values())
 print ("La mayor ganancia ha sido {}".format(mayor_ganancias))
 print ("La menor ganancia ha sido {}".format(menor_ganancias))

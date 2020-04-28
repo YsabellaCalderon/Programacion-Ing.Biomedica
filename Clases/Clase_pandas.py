34481b7f34649866125c111eab1efb7514322f89
YsabellaCalderon <dariannac.briceno@gmail.com>
2020-04-28 10:41:15 -0500

    Pandas

---
+++ Clases/Clase_pandas.py
@@ -0,0 +1,12 @@
+import pandas as p
+diccionario =p.read_csv("balance.csv",encoding = 'UTF-8',header = 0,delimiter=';').to_dict()
+
+nombre_largo = max (diccionario["Ciudad"].values(),key = len)
+nombre_corto = min (diccionario["Ciudad"].values(),key = len)
+print ("\n\n La ciudad con el nombre ,mas largo es {} y la ciudad con el nombre mas corto es {}"
+.format(nombre_largo,nombre_corto))
+
+mayor_ganancias = max (diccionario["Ganancias"].values())
+menor_ganancias = min (diccionario["Perdidas"].values())
+print ("La mayor ganancia ha sido {}".format(mayor_ganancias))
+print ("La menor ganancia ha sido {}".format(menor_ganancias))
---
+++ Clases/diccionario.py
@@ -0,0 +1,22 @@
+diccionario = {}
+medios_transporte = {}
+medios_transporte = {
+    "carros" :
+    [ 
+        "BMW" , 
+        "Audi" ,
+        "Mazda" ,
+        "Kia" ] }
+
+medios_transporte = {
+    "motos" :
+    [
+        "AKT" ,
+        "HONDA" , 
+        " dsak" ] }
+
+print ("Los elementos que componen la llave carros son:{}"
+.format (medios_transporte["carros"]))
+
+print ("Los elementos que componen la llave motos son:{}"
+.format (medios_transporte["motos"]))
